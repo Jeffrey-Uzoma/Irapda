a3f07fbbcddee328716690c4057a290100d2f21a
Zhxade <141938142+Jeffrey-Uzoma@users.noreply.github.com>
2025-10-18 12:22:18 +0100

    Initial commit - complete project upload

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -77,7 +77,7 @@ function Login() {
               className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none pr-10"
               required
             />
-            {/* 👁️ Eye icon */}
+            {/* Eye icon */}
             <button
               type="button"
               onClick={() => setShowPassword(!showPassword)}
